f0a90899c25c0dfbd9f0b49d48b91c2c644e1d7e
Jaeder <jaeder42@gmail.com>
2022-02-11 22:37:52 +0100

    basic guess logic

--- app/controllers/GuessController.scala
+++ app/controllers/GuessController.scala
@@ -6,7 +6,6 @@ import play.api.mvc._
 import play.libs.Json
 
 case class Target(target: String)
-case class Guess(guess: String)
 case class Result(result: String)
 
 @Singleton
@@ -15,20 +14,22 @@ class GuessController @Inject() (val controllerComponents: ControllerComponents)
 
   def index() = Action(parse.json) { request =>
     val message = request.body
-    val requestGuess = (message \ "guess").as[String]
-    val guess = Guess(guess = requestGuess)
+    val guess = (message \ "guess").as[String]
 
     val targetTest = Target(target = "ff00ff")
 
     val targetArray = targetTest.target.split("")
-    val guessArray = guess.guess.split("")
+    val guessArray = guess.split("")
     val resultArray = (1 to 6 map (_ => "0")).toArray
     guessArray.zipWithIndex.foreach { case (e, i) =>
       if (e.equals(targetArray(i))) {
         println(e, i)
         resultArray(i) = "X"
-      } else if (targetArray.indexOf(e) > -1) {
-        // resultArray(i) = "x"
+      } else if (
+        targetArray
+          .indexOf(e) > -1 && !e.equals(guessArray(targetArray.indexOf(e)))
+      ) {
+        resultArray(i) = "x"
       }
     }
     val result = Result(result = resultArray.mkString)
